7c6e5761592c3e8d0c8a2785a1ea23135a61b89b
home <home@192.168.1.12>
2014-04-28 18:09:23 +0900

    경화놋북으로 한번 업뎃을!!

--- src/com/kitri/util/db/DBInfo.java
+++ src/com/kitri/util/db/DBInfo.java
@@ -6,4 +6,6 @@ public class DBInfo {
 	public static final String DB_URL = "jdbc:oracle:thin:@192.168.12.109:1521:orcl";
 	public static final String DB_USER = "kitri7";
 	public static final String DB_PWD = "kitri7";
+	
+	// DB정보는 그날그날 그상황에 맞는 서버로 세팅합시다~~
 }
