254d66edcd97639f5997b1043aebd432288e08c2
musiczoa <usefulhkh@hanmail.net>
2014-05-08 15:28:21 +0900

    db 정보 강사님컴으로 수정

--- src/com/kitri/util/db/DBInfo.java
+++ src/com/kitri/util/db/DBInfo.java
@@ -3,11 +3,11 @@ package com.kitri.util.db;
 // 데이터베이스 정보
 public class DBInfo {
 	public static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
-	public static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:orcl";
+	public static final String DB_URL = "jdbc:oracle:thin:@192.168.12.100:1521:orcl";
 //	public static final String DB_URL = "jdbc:oracle:thin:@180.233.229.139:1521:orcl";
 	// 재훈오빠꺼    180.233.229.139
-	public static final String DB_USER = "kitri";
-	public static final String DB_PWD = "kitri";
+	public static final String DB_USER = "kitri7";
+	public static final String DB_PWD = "kitri7";
 	
 //	public static final String DB_USER = "kitri04";
 //	public static final String DB_PWD = "kitri04";
